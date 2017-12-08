e45166595c15667cee96fded77b0a6411d41e234
Evan Krambuhl <thekrambuhl@gmail.com>
2017-12-07 21:32:23 -0800

    add knobs!

--- source/elements/ActionButton/ActionButton.story.js
+++ source/elements/ActionButton/ActionButton.story.js
@@ -1,18 +1,58 @@
 import { storiesOf } from '@storybook/vue'
+import Centered from '@storybook/addon-centered'
+import { action } from '@storybook/addon-actions'
+
+import {
+  withKnobs,
+  text,
+  select
+} from '@storybook/addon-knobs'
+
 import ActionButton from './ActionButton'
 
-storiesOf('Actions/ActionButton', module)
-  .add('Primary button', () => ({
+const stories =
+  storiesOf('Elements/ActionButton', module)
+    .addDecorator(withKnobs)
+    .addDecorator(Centered)
+
+stories.add('~Knoby ActionButton', () => {
+  const variant = select('Variant', ['primary', 'secondary', 'cta'], 'primary')
+  const href = text('Href', 'http://google.com')
+  const content = text('Content', 'Go to this place')
+
+  return {
+    components: {
+      ActionButton
+    },
+    methods: {
+      onClick ($ev) {
+        action('@click')($ev)
+      }
+    },
+    template: `
+      <ActionButton
+        variant="${variant}"
+        href="${href}"
+        @click="onClick"
+      >
+        ${content}
+      </ActionButton>
+    `
+  }
+})
+
+stories
+  .add('Primary Actionbutton', () => ({
     render: (h) => (
       <ActionButton>Lorem Ipsum</ActionButton>
     )
   }))
-  .add('Secondary button', () => ({
+  .add('Secondary ActionButton', () => ({
     render: (h) => (
       <ActionButton variant="secondary">Lorem Ipsum</ActionButton>
     )
   }))
-  .add('Call-to-action button', () => ({
+  .add('Call-to-action ActionButton', () => ({
     render: (h) => (
       <ActionButton variant="cta" href="#">Lorem Ipsum</ActionButton>
     )
