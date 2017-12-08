7b89697e19265c25f49b084d161b4b64bcbf9169
Evan Krambuhl <thekrambuhl@gmail.com>
2017-12-07 20:28:10 -0800

    working components!

--- source/elements/ActionButton/ActionButton.story.js
+++ source/elements/ActionButton/ActionButton.story.js
@@ -1,14 +1,19 @@
-
 import { storiesOf } from '@storybook/vue'
 import ActionButton from './ActionButton'
 
 storiesOf('Actions/ActionButton', module)
-  .add('Primary button (default)', (h) => (
-    <ActionButton>Lorem Ipsum</ActionButton>
-  ))
-  .add('Secondary button', (h) => (
-    <ActionButton variant="secondary">Lorem Ipsum</ActionButton>
-  ))
-  .add('Call-to-action button', (h) => (
-    <ActionButton variant="cta" href="#">Lorem Ipsum</ActionButton>
-  ))
+  .add('Primary button', () => ({
+    render: (h) => (
+      <ActionButton>Lorem Ipsum</ActionButton>
+    )
+  }))
+  .add('Secondary button', () => ({
+    render: (h) => (
+      <ActionButton variant="secondary">Lorem Ipsum</ActionButton>
+    )
+  }))
+  .add('Call-to-action button', () => ({
+    render: (h) => (
+      <ActionButton variant="cta" href="#">Lorem Ipsum</ActionButton>
+    )
+  }))
